24f03404377c5d4f6890b7de80bb90ed6e7df7a4
Hao Yee <haoyee.dev@gmail.com>
2017-08-14 22:31:04 +1000

    Add test file for location insights

---
+++ test/testGetInsightsByLocationId.js
@@ -0,0 +1,16 @@
+let GoogleMyBusiness = require("../index");
+
+const TOKENS_FILE = "./tokens.json";
+const MANAGER_FILE = "./managers.json";
+const SECRET_FILE = "./client_secret.json";
+
+let gmb = new GoogleMyBusiness(SECRET_FILE, TOKENS_FILE, MANAGER_FILE);
+
+let locationId = "<LOCATION_ID>";
+gmb.getInsightsByLocation(locationId, 1, "week", function (err, v) {
+    if (err) {
+        console.log(`Error: ${JSON.stringify(err)}`);
+    } else {
+        console.log(JSON.stringify(v));
+    }
+});
